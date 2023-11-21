eaacfa277ac9b858c4e595d041f726808956cbf9
iveAlexa <201902471@est.umss.edu>
2023-11-21 08:20:26 -0400

    add the first files

---
+++ src/main/java/com/project/rolmodel/ProjectRolmodelApplication.java
@@ -0,0 +1,13 @@
+package com.project.rolmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRolmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRolmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/rolmodel/ProjectRolmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rolmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRolmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
